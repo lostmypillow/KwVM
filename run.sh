787eaaf5c09cb789e7df8a3f47964d4270d253de
Johnny <jmlin0101@gmail.com>
2025-02-26 21:10:36 +0800

    changed to freesimplegui and added better filtering

--- run.sh
+++ run.sh
@@ -31,13 +31,7 @@ echo "SETUP [Building executable..]"
 pyinstaller --onefile --noconsole --noconfirm --hidden-import proxmoxer.backends --hidden-import proxmoxer.backends.https --hidden-import proxmoxer.backends.https.AuthenticationError --hidden-import proxmoxer.core --hidden-import proxmoxer.core.ResourceException --hidden-import subprocess.TimeoutExpired --hidden-import subprocess.CalledProcessError --hidden-import requests.exceptions --hidden-import requests.exceptions.ReadTimeout --hidden-import requests.exceptions.ConnectTimeout --hidden-import requests.exceptions.ConnectionError vdiclient.py
 echo "ok"
 
-echo "SETUP [Modifying ini..]"
+echo "SETUP [Modifying ini and launching..]"
 python3 -m modify_ini
 echo "ok"
 
-echo "[DONE!] Launching executable..."
-cd dist
-sudo mv vdiclient ~/Desktop/vdiclient
-cd ~/Desktop
-sudo chmod +x vdiclient
-./vdiclient
